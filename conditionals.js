fd3421c6e671ab9776dd0cc1794531d76155cb76
Noah Vandenberg <noah@noahvandenberg.com>
2021-12-20 10:53:34 -0800

    Add more conditional examples

--- conditionals.js
+++ conditionals.js
@@ -8,4 +8,8 @@ if (temperature < 0) {
   console.log("Short sleeves are fine.");
 }
 
-console.log("Now you're ready to go outside!");
+if (temperature < -40 || temperature > 40) {
+  console.log("Maybe going outside isn't such a great idea…");
+} else {
+  console.log("Now you're ready to go outside!");
+}
